52029c96b486c3b68627d544bb4e60ffae72c70f
Mhummad Mutahir <121317971+MRMutahir@users.noreply.github.com>
2023-04-25 02:46:37 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,444 @@
+// alert("Hello World")
+// alert("How are you")
+// var amir = ("how are you"); 
+// amir="why you come";
+// alert (amir);
+// amir="what are you doing now amiir"
+// var mutahir =('how are you ');
+// alert (mutahir)
+// var studentcome=('how many student come ');
+// studentcome='student are not come';
+// var studentAbsent=('how many student absent ');
+// var mutahuir_come=('mutahuir is here ');
+// alert(studentcome+studentAbsent+mutahuir_come);
+// alert(studentAbsent);
+// alert(mutahuir_come);
+// var firstNumber =50+200;
+// var secondNumber =100;
+
+// alert (firstNumber+secondNumber-20)
+// swal("A wild Pikachu appeared! What do you want to do?", {
+//     buttons: {
+//       cancel: "Run away!",
+//       catch: {
+//         text: "Throw Pokéball!",
+//         value: "catch",
+//       },
+//       defeat: true,
+//     },
+//   })
+//   .then((value) => {
+//     switch (value) {
+
+//       case "defeat":
+//         swal("Pikachu fainted! You gained 500 XP!");
+//         break;
+
+//       case "catch":
+//         swal("Gotcha!", "Pikachu was caught!", "success");
+//         break;
+
+//       default:
+//         swal("Got away safely!");
+//     }
+//   });
+//  var  formThankYou =('thankb you');
+//  var  formInput =("Mutahir");
+//  alert(formThankYou);
+//  alert(formInput);
+//  var qulification = prompt ("What is your name") 
+//  alert(qulification)
+//  var totalStudent =(100)
+//  var presentStudent = prompt('How Many Student present')
+//  var absentStudent = prompt('How Many Student absent')
+//  var totalNum =(presentStudent+absentStudent)
+//  alert (totalNum)
+
+//  const firstNumber=(5)
+//  const firstName=(5)
+//  alert(firstName+firstNumber)
+// alert('Islam is good and everytime' )
+// var firstName =("Mutahir"+" "+"kareem")
+// alert(firstNamer)
+// var name =("Mutahir")
+// var lastName =(" Kareem")
+// var age =("18")
+// alert(name+lastName+age)
+// alert(' well Come \n Mutahir')
+// var presentStudent=+prompt('how many presentStudent?')
+// var absentStudent=+prompt('how many absentStudent?')
+// console.log(presentStudent+absentStudent)
+// if(true){
+//     alert (presentStudent-absentStudent)
+// }
+// var num=('786 ')
+// var loremTextFirst=('\n  My name is Mutahir m ek bhut bura hon ALLAH PAK mujh maff krde mere eman ki hifazatn   \n farmay hmsab k eman ki hifazt farmay')
+// var hello=( ' Hello world')
+// alert(num+hello+loremTextFirst)
+// var name=('Mutahir');
+// var age=(18);
+// var text=('INSHALLAH microsft and Doctor');
+// alert(name);
+// alert(age);
+// alert(text);
+// var iAm=('I am ')
+// var name =('Mutahir')
+// alert(iAm+name)
+// var i_Am =("i am ")
+// var age=(  '18' )
+// i_Am=("i am  year old")
+// alert(i_Am+age)
+// var visite=("i am visite this site ")
+// var ok=('14 time')
+// alert(visite+ok)
+// var num1=(3);
+// var num_2=(10);
+// var num_string=(100);
+// var num_3=(2);
+// var num_4=(6);
+// alert(num1+num_2+num_3+num_4+num_string)
+// var day=prompt("tell me Exam suchdul");
+// if(day == 'monday'){
+//     alert('english')
+// }else if (day == 'tuesday'){
+//     alert('urdu')
+// }else if (day == 'wednesday'){
+//     alert('Math')
+// } else {
+//     alert('free')
+// }
+
+// 
+// var day = prompt("Which day's menu , you want to know??")
+
+// if (day == "monday") {
+//     console.log("murgi ki karahi")
+// } else if (day == "tuesday") {
+//     console.log("sabzi")
+// } else if (day == "wednesday") {
+//     console.log("sabzi")
+// } else if (day == "thursday") {
+//     console.log("murgi")
+// } else if (day == "friday") {
+//     console.log("biryani")
+// } else if (day == "saturday") {
+//     console.log("daal chawal")
+// } else {
+//     console.log("holiday")
+// }
+// var firstName=prompt('what is your \n first name');
+// alert(firstName);
+// var wellCome=('well come'+"       " +  firstName)
+// alert(wellCome)
+// var giveInPut=prompt('give your \n email addrees');
+// var num_1 =+prompt('Give your \n cureent age');
+// var num_2 =+prompt('give your before 5 year age');
+// var firstName=prompt('what is \n your name ');
+// var lastName=prompt('what is your  \nlast name');
+// alert(giveInPut);
+// alert(num_1-num_2)
+// // alert (firstName+lastName)
+// if(num_1=='50'){
+//     alert('your are in golden age')
+// } else if(num_1=='18'){
+//     alert('youngstuners')
+// }else if(num_1=="70"){
+//     alert('end of time')
+// }else (
+//     alert('come ture way islam')
+// )
+// var total=(100);
+// var firstTime=(60);
+// // var ekor=(20);
+// var secondTime=(40);
+// if(total>firstTime + secondTime){
+//     alert('salrydone')
+// }
+
+
+// if (total !== firstTime+secondTime){
+//     alert('sorry sir')
+// }
+// var num_1=(2)
+// var num_2=(2)
+// var num_3=(2)
+// var num_4=(2)
+//  alert(num_1++)
+//  alert(num_2--)
+//  alert(++num_3)
+//  alert(--num_4)
+// var Menu =prompt('Aj konsa din hen')
+// if(Menu=='Monday'||'tuesday'){
+//     alert('Biryani')
+// }else if ( Menu=='sunday'+'wednesday'){
+//     alert('off hen aj')
+// }else if(Menu=='friday'){
+//     alert("ghr s kha kr ao")
+// }else{
+//     alert('dal chawal')
+// }
+// var num1=+prompt('Number1')
+// var num2=prompt("op")
+// var num3=+prompt('Number2')
+// if(num2=='+'){
+//     alert(num1+num3)
+// }if(num2=='-')
+// {alert(num1-num3)
+// }
+// function student(num){
+//     console.log("${liquid}"
+//     console.log('Aj 50   nh ay')
+//     console.log('Aj 50 students nh ay')
+//     console.log('Aj 50 students nh ay')
+// }
+// student(students)
+// student(teacher)
+// student(madams)
+// student(staff)
+
+// var num_1=+prompt('Wriet Number')
+// var num_2=+prompt('Wriet Number')
+
+// function resultOfTheYear(){
+//     if (num_1===num_2) {
+//         alert("First Position")
+
+//     }else if (num_1==70||60) {
+//         alert('second position')
+
+//     }else if (num_1==50||55) {
+//         alert('third Position')
+
+//     }else(alert('Fail'))
+// }
+// 
+
+// var num_1=+prompt("Number1");
+// var oprater=prompt("Oprater");
+// var num_2=+prompt("Number2");
+// if (oprater=="+") {
+//     // console.log(num_1+num_2)
+//     result=num_1+num_2
+
+// }
+// if (oprater=="-") {
+//     // console.log(num_1-num_2)
+//     result=num_1-num_2
+
+// }
+// if (oprater=="/") {
+//     // console.log(num_1*num_2)
+//     result=num_1/num_2
+
+// }
+// alert(result)
+// var firstEmpoly='Mutahir'
+// var sallry=500
+// sallry=600
+// var Firsttime=200
+// var secondtime=300
+// if (sallry==Firsttime+secondtime) {
+//     alert('sorry')
+
+// }
+// if (sallry!==Firsttime+secondtime) {
+//     alert('milegijygi')
+
+// }
+//  function Mutahir(a,b) {
+//     return(`${a}kia  hal hen`)
+//  }
+// alert(Mutahir("SALAM ")) 
+//  document.write  (Mutahir("Hi,Mutahir "))
+// function schhol(a,b) {
+//    console.log(`${b}\n Well come back to School \n${a}`)
+
+// }
+// schhol('Mutahir','taha')
+// // es6
+// const  school=(a,b)=>{
+//    return`${a}mujhe pani do${b}`
+
+// }
+// console.log(school('taha','apply juice ')) 
+
+// var a=2
+// var b=4
+// var c=6
+// var d=8
+// let number_sy=()=> a+b
+// console.log(number_sy(a,b))
+
+// JavaScript implementation of
+// the above approach
+
+// function hottel(a, b, c) {
+// return(hottel)
+
+// }
+// console.log(`aj hottel m  ${a}hen  khane  m `)
+// console.log(`monday ko ${b} tha khane m`)
+// console.log(`${c} kb milegi`)
+
+// hottel('Mantan')
+
+// let mutahir=(a,b)=>{
+//    alert(`aj m nh araha hon  ${a}`)
+//    alert(`aj m aonga ${b}`)
+
+// }
+// mutahir('taha','raza')
+//    let x=10;
+//    let y=5;
+// let addNumber=(a,b)=>x+y
+// console.log(x+y)
+// 'dog','cat','birds'.'lion','parot','ant'
+
+//  let cars = ['civi', 'BMW','Swift','Khebar', 'alto',]
+//  console.log(cars)
+//  let pets=[]
+//  pets[0]='dog';
+//  pets[6]='cat';
+//  console.log(pets) 
+//  const pets_2=['dog','cat','birds','lion','parot','ant']
+//  pets_2.shift(1,4)
+//  console.log(pets_2)
+// Well come Islamic Habbit
+// else if (userName!=='Mutahir'||'taha'||'Raza') {
+//    console.log('Sorry Enter your corect name')  
+// }else if (userEmail!=='tahakareem820@gmail.com'||'mutahirkareem820@gmail.com'||'razaanwar820@gmail.com') {
+//    console.log('Sorry Enter your coreect email')
+
+// var userName=prompt('Enter your Name');
+// var userEmail=prompt('Enter your Email')
+// var sumEmailName=(userName+userEmail)
+// if (sumEmailName=='Mutahir'&&'tahakareem820@gmail.com'||'taha'||'raza') {
+//    console.log('Done')  
+// }
+// var pets=[]
+// pets[0]='dog'
+// pets[5]='cat'
+
+// console.log(pets)
+// var freeLancerMarketplace=['fiveer','guru','upwork','workchest']
+//    let exChange= freeLancerMarketplace.pop()
+//    alert(exChange)
+//  console.log(freeLancerMarketplace)
+
+//  var cars=['bmw', 'swift','civic','khyber',]
+//   let  carsExChange= cars.push('mheran')
+//   console.log(cars)
+//   console.log(carsExChange)
+
+
+//   var colorBox=['Green','Brown','Pink','White']
+//   let color$Box=colorBox.shift();
+//   console.log(colorBox)
+//   var seaVeiw=['Naran','Gagan','Malir']
+//    let sea_Veiw=seaVeiw.unshift('h')
+//    console.log(seaVeiw)
+//    console.log(sea_Veiw)
+//     var numbers=[2,4,6,8,10]
+//     let plusnum=numbers.toString()
+//     console.log(plusnum,)
+//     console.log( typeof plusnum)
+//      var  myFriends=['raza','abdullah','yaseen','yameen']
+//      var   mybrothers=['talha','Mutahir','Eshareeb','taha']
+//      console.log(myFriends.concat(mybrothers))
+// let exam = [50,60,70,80,90]
+// console.log(exam)
+// console.log(exam.length)
+//  let  showNumber = exam.indexOf(90)
+//  console.log(showNumber) 
+// console.log(exam.push(100))
+// console.log(exam)
+// console.log(exam.pop())
+// console.log(exam)
+// console.log(exam.unshift('Mutahir'))
+// console.log(exam)
+// console.log(exam.shift())
+// console.log(exam)
+
+//  let fruit = ['Apply','Banana','Pineapply','Orange']
+// console.log(fruit)
+// console.log(Array.isArray('Banana'))
+// let student= ['Asim','Asad']
+// student[8]= 'Muitahir'
+// student[6]= 'Raza'
+// student[4] = 'abdullah'
+// console.log(student)
+// var time=2;
+// if(time > 1){console.log("good morning")}
+// 
+// Thu Mar 30 2023 16:51:06 GMT+0500 (Pakistan Standard Time)   
+
+
+// let date = new Date ();
+// let day = date.getDate();
+// console.log(day);
+
+
+//  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+//  let date = new Date ();
+//  let nowaDays  = date.getDay()
+//  let right = days[nowaDays]
+//  console.log(right)
+// let hours = date.getMonth()
+// console.log(hours)
+
+// let days2 = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+// let ajkidate = new Date()
+// let ajkidin = ajkidate.getDay()
+// let now = days2[ajkidin]
+// console.log(now)
+
+let main = document.querySelector('.main');
+let same = document.querySelectorAll('.same');
+let child1 = document.querySelectorAll('.child1');
+let child2 = document.querySelectorAll('.child2');
+let child3 = document.querySelectorAll('.child3');
+let change = document.querySelector('.change');
+let btnX = document.querySelector('.btnX');
+// let on_off = document.querySelector('.off')
+
+// console.log(on_off)
+
+let color = ['red', "green", 'yellow', 'pink',];
+let xcolor = ['#A3CB38', '#12CBC4', '#9980FA', '#FFC312', '#F79F1F', '#0652DD']
+// const element = color[i];
+
+
+console.log(change);
+console.log(same);
+console.log(main);
+// console.log(number)
+change.addEventListener('click', (() => {
+   let number = getNum()
+   console.log(number);
+   main.style.backgroundColor = color[number];
+   // active_light.style.display = 'block';
+
+}))
+btnX.addEventListener('click',()=>{
+   let xNum = getXnum() ;
+   console.log(xNum);
+   main.style.backgroundColor = xcolor[xNum];
+   
+})
+function getXnum (){
+   return Math.round(Math.random()* xcolor.length)
+}
+
+function getNum() {
+   return Math.round(Math.random() * color.length); 
+
+}
+let trouchBtn = document.querySelector('.trouch-btn');
+let active_light= document.querySelector('.active_light');
+console.log(active_light) ;
+trouchBtn.addEventListener('click',()=>{
+   document.querySelector('.active_light').classList.toggle('off')
+   // active_light.style.display = 'none'
+})
+
+
